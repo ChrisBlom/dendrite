4d566da422f589849504cf9dcec0fadb91f13da3
Chris Blom <chris.blom@millmobile.com>
2015-09-10 22:15:33 +0200

    Two circles

--- window.scm
+++ window.scm
@@ -1,10 +1,20 @@
 (import chicken scheme)
+
 (use (prefix glfw3 glfw:)
      (prefix opengl-glew gl:)
      gl-math
      gl-utils
      srfi-18 ; threads
-     box)
+     srfi-42 ; eager comprehension
+     box
+     nrepl)
+
+(define (range init limit #!optional [step 1])
+  (if (>= init limit)
+      '()
+      (cons init (range (+ init step) limit step ))))
+
+(define (inc i) (+ 1 i))
 
 (define *vertex*
 #<<END
@@ -27,61 +37,171 @@ END
 in vec3 c;
 out vec4 fragColor;
 void main(){
-  fragColor = vec4(c, 1.0);
+  fragColor = vec4(c, 0.5);
 }
 END
 )
 
+(define (circle-positions n)
+  (apply append (cons '(0 0)
+		      (map (lambda (i)
+			     (let ([angle (/ (* pi 2 i) n)])
+			       (list (sin angle) (cos angle))))
+			   (range 0 n)))))
+
+(define (circle-colors n)
+  (apply append (cons '(255 0 255)
+		      (map (lambda (i)
+			     (let ([h (modulo i 2)])
+			       (map (compose (cut * <> 255.))
+				    (list h
+					  h
+					  h))))
+			   (range 0 n 1)))))
+
+(define (circle-indices n)
+  (apply append
+	 (map (lambda (i) (list 0
+			   (inc i)
+			   (inc (modulo (inc i) n))))
+		     (range 0 n))))
+
+(define hexagon-positions (circle-positions 6))
+
+(define hexagon-colors (circle-colors 6))
+
+(define hexagon-indices
+  (apply append '((0 1 2)
+		  (0 2 3)
+		  (0 3 4)
+		  (0 4 5)
+		  (0 5 6)
+		  (0 6 1))))
+
+;;
+;;      1_________2
+;;      /         \
+;;     /           \
+;;  6 /     0.      \3
+;;    \            /
+;;     \          /
+;;      \________/
+;;     5          4
+(define hex (make-mesh
+              vertices: `(attributes: ((position #:float 2)
+                                       (color #:unsigned-byte 3
+					      normalized: #t))
+                          initial-elements: ((position . ,hexagon-positions)
+                                             (color . ,hexagon-colors)))
+              indices: `(type: #:ushort
+			       initial-elements: ,hexagon-indices)))
+
+(define (disk n)
+  (make-mesh
+   vertices: `(attributes: ((position #:float 2)
+			    (color #:unsigned-byte 3
+				   normalized: #t))
+			   initial-elements: ((position . ,(circle-positions n))
+					      (color . ,(circle-colors n))))
+   indices: `(type: #:ushort
+		    initial-elements: ,(circle-indices n))))
+
+;;   0     ->              1
+;;  -1,-1               1,-1
+;;     _________________
+;;    |                |
+;;    |                |
+;;    |                |
+;;    |                |   |
+;;    |                |   v
+;;    |                |
+;;    |                |
+;;    |                |
+;;    |________________|
+;;  -1,1     <-          1,1 2					;
+;; 3
 (define rect (make-mesh
               vertices: '(attributes: ((position #:float 2)
-                                       (color #:unsigned-byte 3
+                                       (color #:float 3
                                               normalized: #t))
                           initial-elements: ((position . (-1 -1
                                                            1 -1
                                                            1  1
-                                                           -1  1))
-                                             (color . (255 0   0
+                                                           -1  1
+
+							   ))
+                                             (color . (255 255 0
                                                        0   255 0
                                                        0   0   255
                                                        255 0   255))))
               indices: '(type: #:ushort
                          initial-elements: (0 1 2
-                                            0 2 3))))
+					      0 2 3))))
 
 (define program (make-parameter #f))
 
-(define projection-matrix
-  (perspective 640 480 0.1 100 70))
+(define (projection-matrix)
+  (perspective 800 600 0.1 100 70))
 
 (define d (make-box 5.0))
+
 (define r (make-box 0.0))
 
 (define (eye)
-  (make-point (* (box-ref d) (sin (box-ref r)))
+  (make-point 0 ;(* (box-ref d) (sin (box-ref r)))
 	      0
-	      (* (box-ref d) (cos (box-ref r)))))
+	      10; (* (box-ref d) (cos (box-ref r)))
+	      ))
 
 (define (view-matrix)
   (look-at (eye)
            (make-point 0 0 0)
            (make-point 0 2 0)))
 
-(define model-matrix (mat4-identity))
+(define (model-matrix) (mat4-identity))
+
+(define shape (disk 100))
+
 
 (define (render)
-  (gl:use-program (program))
-  (gl:bind-vertex-array 0)
-  (gl:uniform-matrix4fv (gl:get-uniform-location (program) "MVP")
-                        1 #f
-                        (m* projection-matrix
-                            (m* (view-matrix)
-				model-matrix)))
-  (gl:bind-vertex-array (mesh-vao rect))
-  (gl:draw-elements-base-vertex (mode->gl (mesh-mode rect))
-                                (mesh-n-indices rect)
-                                (type->gl (mesh-index-type rect))
-                                #f 0)
-  (check-error))
+  (let ([mvp (m* (projection-matrix)
+		 (m* (view-matrix)
+		     (model-matrix)))]
+
+	[mvp2 (m* (projection-matrix)
+		  (m* (view-matrix)
+		      (translate (make-point (sin (box-ref r))
+					     (cos (box-ref r))
+					     0)
+				 (model-matrix))))]
+	)
+    ;; shaders
+    (gl:use-program (program))
+
+
+    (gl:bind-vertex-array 0)
+    (gl:uniform-matrix4fv (gl:get-uniform-location (program) "MVP")
+			  1 #f
+			  mvp)
+    (gl:bind-vertex-array (mesh-vao shape))
+    (gl:draw-elements-base-vertex (mode->gl (mesh-mode shape))
+				  (mesh-n-indices shape)
+				  (type->gl (mesh-index-type shape))
+				  #f 0)
+
+    (gl:bind-vertex-array 0)
+    (gl:uniform-matrix4fv (gl:get-uniform-location (program) "MVP")
+    			  1 #f
+    			  mvp2)
+    (gl:bind-vertex-array (mesh-vao shape))
+
+    ;; draw shape
+    (gl:draw-elements-base-vertex (mode->gl (mesh-mode shape))
+    				  (mesh-n-indices shape)
+    				  (type->gl (mesh-index-type shape))
+    				  #f 0)
+
+    (check-error)))
 
 (glfw:key-callback (lambda (window key scancode action mods)
                      (cond
@@ -89,23 +209,28 @@ END
                        (glfw:set-window-should-close window #t)])))
 
 (define (main)
-  (glfw:with-window
-   (640 480 "Example"
-	resizable: #f
-	context-version-major: 3
-	context-version-minor: 2
-	opengl-forward-compat: #t
-	opengl-profile: glfw:+opengl-core-profile+
-	)
+  (glfw:with-window (800 600 "Example"
+			 resizable: #f
+			 context-version-major: 3
+			 context-version-minor: 2
+			 opengl-forward-compat: #t
+			 opengl-profile: glfw:+opengl-core-profile+)
    (gl:init)
-					; (print (gl:supported? "GL_ARB_framebuffer_object"))
+   ;; (print (gl:supported? "GL_ARB_framebuffer_object"))
+
    (set! *vertex* (make-shader gl:+vertex-shader+ *vertex*))
+
    (set! *fragment* (make-shader gl:+fragment-shader+ *fragment*))
+
+   ;; compile shader, set program parameter
    (program (make-program (list *vertex* *fragment*)))
-   (mesh-make-vao! rect `((position . ,(gl:get-attrib-location
+
+   ;; create shape vertex array opbject
+   (mesh-make-vao! shape `((position . ,(gl:get-attrib-location
 					(program) "position"))
 			  (color . ,(gl:get-attrib-location
 				     (program) "color"))))
+
    (let loop ([i 0])
      (glfw:swap-buffers (glfw:window))
      (gl:clear (bitwise-ior gl:+color-buffer-bit+ gl:+depth-buffer-bit+))
@@ -116,7 +241,13 @@ END
        (box-swap! r + 0.01)
        (loop (+ .01 i))))))
 
+
+;(use nrepl)
+;(define nrepl-thread (thread-start! (lambda () (nrepl 1234))))
+
 (define main-thread (thread-start! (make-thread (lambda () (print 'main) (main)))))
+
 (repl)
+
 
 ;; run (use box) again in the repl
