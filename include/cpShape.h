2f7d716e951d56b458fd00b5a11c4cc79e46c8c6
Chris Blom <chris.blom@millmobile.com>
2015-10-23 20:46:10 +0200

    Comment out structs

--- include/cpShape.h
+++ include/cpShape.h
@@ -24,43 +24,43 @@
 /// @{
 
 /// Point query info struct.
-typedef struct cpPointQueryInfo {
-	/// The nearest shape, NULL if no shape was within range.
-	const cpShape *shape;
-	/// The closest point on the shape's surface. (in world space coordinates)
-	cpVect point;
-	/// The distance to the point. The distance is negative if the point is inside the shape.
-	cpFloat distance;
-	/// The gradient of the signed distance function.
-	/// The value should be similar to info.p/info.d, but accurate even for very small values of info.d.
-	cpVect gradient;
-} cpPointQueryInfo;
-
-/// Segment query info struct.
-typedef struct cpSegmentQueryInfo {
-	/// The shape that was hit, or NULL if no collision occured.
-	const cpShape *shape;
-	/// The point of impact.
-	cpVect point;
-	/// The normal of the surface hit.
-	cpVect normal;
-	/// The normalized distance along the query segment in the range [0, 1].
-	cpFloat alpha;
-} cpSegmentQueryInfo;
-
-/// Fast collision filtering type that is used to determine if two objects collide before calling collision or query callbacks.
-typedef struct cpShapeFilter {
-	/// Two objects with the same non-zero group value do not collide.
-	/// This is generally used to group objects in a composite object together to disable self collisions.
-	cpGroup group;
-	/// A bitmask of user definable categories that this object belongs to.
-	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
-	cpBitmask categories;
-	/// A bitmask of user definable category types that this object object collides with.
-	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
-	cpBitmask mask;
-} cpShapeFilter;
-
+////// typedef struct cpPointQueryInfo {
+////// 	/// The nearest shape, NULL if no shape was within range.
+////// 	const cpShape *shape;
+////// 	/// The closest point on the shape's surface. (in world space coordinates)
+////// 	cpVect point;
+////// 	/// The distance to the point. The distance is negative if the point is inside the shape.
+////// 	cpFloat distance;
+////// 	/// The gradient of the signed distance function.
+////// 	/// The value should be similar to info.p/info.d, but accurate even for very small values of info.d.
+////// 	cpVect gradient;
+////// } cpPointQueryInfo;
+//////
+////// /// Segment query info struct.
+////// typedef struct cpSegmentQueryInfo {
+////// 	/// The shape that was hit, or NULL if no collision occured.
+////// 	const cpShape *shape;
+////// 	/// The point of impact.
+////// 	cpVect point;
+////// 	/// The normal of the surface hit.
+////// 	cpVect normal;
+////// 	/// The normalized distance along the query segment in the range [0, 1].
+////// 	cpFloat alpha;
+////// } cpSegmentQueryInfo;
+//////
+////// /// Fast collision filtering type that is used to determine if two objects collide before calling collision or query callbacks.
+////// typedef struct cpShapeFilter {
+////// 	/// Two objects with the same non-zero group value do not collide.
+////// 	/// This is generally used to group objects in a composite object together to disable self collisions.
+////// 	cpGroup group;
+////// 	/// A bitmask of user definable categories that this object belongs to.
+////// 	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
+////// 	cpBitmask categories;
+////// 	/// A bitmask of user definable category types that this object object collides with.
+////// 	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
+////// 	cpBitmask mask;
+////// } cpShapeFilter;
+//////
 /// Collision filter value for a shape that will collide with anything except CP_SHAPE_FILTER_NONE.
 //////static const cpShapeFilter CP_SHAPE_FILTER_ALL = {CP_NO_GROUP, CP_ALL_CATEGORIES, CP_ALL_CATEGORIES};
 /// Collision filter value for a shape that does not collide with anything.
